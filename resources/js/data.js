f352ef19c2a4abcf3800557748f276914370badb
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-10-07 17:11:39 -0700

    displayed moods with button using javascript part 1

---
+++ resources/js/data.js
@@ -0,0 +1,75 @@
+const data = [{
+    "id": 1,
+    "name": "happy",
+    "image": "./resources/images/happy_spongebob.gif",
+    "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
+    "price": 5.99
+  }, {
+    "id": 2,
+    "name": 'sad',
+    "image": './resources/images/sad_spongebob.gif',
+    "desc": "Tears are words that can’t be explained.",
+    "price": 5.99
+  }, {
+    "id": 3,
+    "name": 'angry',
+    "image": "./resources/images/angry_1.gif",
+    "desc": "Anger is like a storm rising up from the bottom of your consciousness. When you feel it coming, turn your focus to your breath.",
+    "price": 5.99
+  }, {
+    "id": 4,
+    "name": 'calm',
+    "image": "./resources/images/calm_woman.gif",
+    "desc": "To be calm is the highest achievement of the self.",
+    "price": 5.99
+  }, {
+    "id": 5,
+    "name": 'curious',
+    "image": "./resources/images/curious_owl.gif",
+    "desc": "I have no special talents. I am only passionately curious.",
+    "price": 5.99
+  }, {
+    "id": 6,
+    "name": 'disgust',
+    "image": "./resources/images/disgust_theoffice.gif",
+    "desc": "Not all disgust need produce symptoms of nausea, nor all nausea mark the presence of disgust.",
+    "price": 5.99
+  }, {
+    "id": 7,
+    "name": 'energetic',
+    "image": "./resources/images/energetic_puppy.gif",
+    "desc": "Be the energy you want to attract.",
+    "price": 5.99
+  }, {
+    "id": 8,
+    "name": 'gloomy',
+    "image": "./resources/images/gloomy_girl.gif",
+    "desc": "All was black, gloomy and awful. There was no light at the end of the tunnel - or if there was, it was an oncoming train.",
+    "price": 5.99
+  }, {
+    "id": 9,
+    "name": 'scared',
+    "image": "./resources/images/scared_spongebob.gif",
+    "desc": "The only thing we have to fear is fear itself.",
+    "price": 5.99
+  }, {
+    "id": 10,
+    "name": 'shy',
+    "image": "./resources/images/shy_dog.gif",
+    "desc": "When a shy person smiles, it's like the sun coming out.",
+    "price": 5.99
+  }, {
+    "id": 11,
+    "name": 'sleepy',
+    "image": "./resources/images/sleepy_cat.gif",
+    "desc": "Your future depends on your dreams, so go to sleep.",
+    "price": 5.99
+  }, {
+    "id": 12,
+    "name": 'surprised',
+    "image": "./resources/images/surprised_2.gif",
+    "desc": "The only thing that should surprise us is that there are still some things that can surprise us.",
+    "price": 5.99
+  }]
+  
+  export default data;
---
+++ resources/js/scripts.js
@@ -0,0 +1,20 @@
+import data from './data.js'
+const itemsContainer = document.getElementById('items')
+
+// the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+    console.log(img)
+}
